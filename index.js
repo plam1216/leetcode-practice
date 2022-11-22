652efcfb57a971fd1a3518dec881d3e013b99c2e
Peter Lam <plam1216@gmail.com>
2022-11-21 21:23:16 -0500

    Q.58 solution

---
+++ index.js
@@ -0,0 +1,32 @@
+///////////////////////////
+// 58. Length of Last Word
+///////////////////////////
+
+// Input: s = "Hello World"
+// Output: 5
+// Explanation: The last word is "World" with length 5.
+
+// Input: s = "   fly me   to   the moon  "
+// Output: 4
+// Explanation: The last word is "moon" with length 4.
+
+s = "Hello World"
+// s = "   fly me   to   the moon  "
+
+var lengthOfLastWord = function(s) {
+    // split the string by ' ' to get each word
+    // this returns an array
+    let strArr = s.split(' ')
+
+    // filter out all empty spaces ' ' so the array only contains words
+    let wordsArr = strArr.filter(word => word !== '')
+    // console.log(wordsArr)
+
+    // find the length of the last word in the 'wordsArr'
+    let lastWordLength = wordsArr[wordsArr.length-1].length
+    // console.log(lastWordLength)
+
+    return lastWordLength
+};
+
+console.log("Lenght of Last Word: ", lengthOfLastWord(s))
