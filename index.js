dae902ef2a3eb736c578628463c8fcded6459adb
Peter Lam <plam1216@gmail.com>
2022-11-28 15:29:32 -0500

    leetcode 125. valid palindrome

--- index.js
+++ index.js
@@ -133,7 +133,7 @@ var twoSum = function (nums, target) {
     // nested loop to iterate at index 1 higher than "temp"
     // check if adding "temp" and nested loop index of nums gets target sum
     // if it does return indicies
-    
+
     // hold indexes that sum to target
     let twoSum = []
 
@@ -159,4 +159,106 @@ var twoSum = function (nums, target) {
 };
 
 // expected [1, 2]
-console.log("1. Two Sum", twoSum([3,2,4], 6))
+console.log("1. Two Sum", twoSum([3, 2, 4], 6))
+
+
+////////////////////////////////////////
+// 121. Best Time to Sell a Stock (EASY)
+////////////////////////////////////////
+
+// You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+// You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+// Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+// Example 1:
+// Input: prices = [7,1,5,3,6,4]
+// Output: 5
+// Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+// Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+
+// Example 2:
+// Input: prices = [7,6,4,3,1]
+// Output: 0
+// Explanation: In this case, no transactions are done and the max profit = 0.
+
+var maxProfit = function (prices) {
+    // keep track of profit
+    let profit = 0
+
+    // loop through array and compare prices
+    for (let i = 0; i < prices.length; i++) {
+        // temp variable assigned to use as buyDay
+        let temp = prices[i]
+        // loop through array to find best sellDay
+        for (let j = i + 1; j < prices.length; j++) {
+            // if sellDay value is greater than buyDay
+            if (prices[j] > prices[i]) {
+                // let difference equal sellDay - buyDay
+                let difference = prices[j] - temp
+
+                // if difference is greater than current profit, reassign profit
+                if (difference > profit) {
+                    profit = difference
+                }
+            }
+        }
+    }
+    return profit
+};
+
+console.log(maxProfit([7, 1, 5, 3, 6, 4]))
+console.log(maxProfit([7, 6, 5, 4, 3, 1]))
+
+////////////////////////////////
+// 125. Valid Palindrome (EASY)
+////////////////////////////////
+
+// A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+// Given a string s, return true if it is a palindrome, or false otherwise.
+
+// Example 1:
+
+// Input: s = "A man, a plan, a canal: Panama"
+// Output: true
+// Explanation: "amanaplanacanalpanama" is a palindrome.
+// Example 2:
+
+// Input: s = "race a car"
+// Output: false
+// Explanation: "raceacar" is not a palindrome.
+
+let isPalindrome = (s) => {
+    // change all letters in starting array to lowercase and split into individual characters
+    let sArr = s.toLowerCase().split('')
+    
+    // empty array to hold valid characters
+    let palArr = []
+
+    // loop through sArr to get valid characters
+    for(let i = 0; i < s.length; i++) {
+        // if it is a letter between 'a' and 'z' push to array
+        if(sArr[i].charCodeAt(0) >= 97 && sArr[i].charCodeAt(0) <= 122) {
+            palArr.push(sArr[i])
+        }
+
+        // if it is a valid number between '0' and '9' push to array
+        if(sArr[i].charCodeAt(0) >= 48 && sArr[i].charCodeAt(0) <= 57) {
+            palArr.push(sArr[i])
+        }
+    }
+
+    // join the valid array to get the word
+    let word = palArr.join('')
+
+    // reverse the valid array and join to get the palindrome
+    let palWord = palArr.reverse().join('')
+
+    // compare the word and palindrome to see if it is valid
+    return word === palWord
+};
+
+console.log(isPalindrome("A man, a plan, a canal: Panama"))
+console.log(isPalindrome("race a car"))
