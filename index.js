f1bf6207c30a30d4e40fa0c924a7be662eee4e3e
Peter Lam <plam1216@gmail.com>
2022-11-30 14:10:00 -0500

    leetcode numIdenticalPairs

--- index.js
+++ index.js
@@ -456,3 +456,40 @@ let uniqueOccurrences = (arr) => {
 
 console.log("[1,2,2,1,1,3]", uniqueOccurrences([1, 2, 2, 1, 1, 3]))
 console.log()
+
+////////////////////////////////////
+// 1512. Number of Good Pairs (EASY)
+////////////////////////////////////
+
+// Given an array of integers nums, return the number of good pairs.
+// A pair (i, j) is called good if nums[i] == nums[j] and i < j.
+
+// Example 1:
+// Input: nums = [1,2,3,1,1,3]
+// Output: 4
+// Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
+
+// Example 2:
+// Input: nums = [1,1,1,1]
+// Output: 6
+// Explanation: Each pair in the array are good.
+
+// Example 3:
+// Input: nums = [1,2,3]
+// Output: 0
+
+let numIdenticalPairs = (nums) => {
+    let pairs = 0
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] === nums[j]) {
+                pairs++
+            }
+        }
+    }
+    return pairs
+};
+
+console.log("numIdenticalPairs([1, 2, 3, 1, 1, 3]):", numIdenticalPairs([1, 2, 3, 1, 1, 3]))
+console.log("numIdenticalPairs([1, 2, 3, 1, 1, 3]):", numIdenticalPairs([1, 1, 1, 1]))
+console.log()
