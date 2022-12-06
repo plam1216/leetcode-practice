e4c07eea54442c2afefee05fe4ee9c20c4f57dff
Peter Lam <plam1216@gmail.com>
2022-12-05 22:49:29 -0500

    middleNode alternate solution

--- index.js
+++ index.js
@@ -744,14 +744,35 @@ let middleNode = (head) => {
 
     // loop through nodeList, decrement nodeSize by 1 until it equals 'middle' size
     // this gives the resulting middle node
+    // [1, 2, 3, 4, 5] => middle = 3
+    
+    // 5 != 3... 4 != 3... 3 != 3 --- break loop
     while (nodeSize != middle) {
+        // 1 -> 2... 2 -> 3
         middleNode = middleNode.next
+
+        // 5 - 1 = 4... 4 - 1 = 3
         nodeSize--
     }
 
-    // console.log(middleNode)
     return middleNode
 };
+
+// Alternate Solution
+// // Time Complexity :  O(n)
+// // Space Complexity : O(1)
+// let middleNode = (head) => {
+//     // Initialize two pointers p1 and p2 pointing to the head...
+//     var p1 = head;
+//     var p2 = head;
+//     while(p2 != null && p2.next != null){
+//         // In each iteration, we move p1 one node forward and we move p2 two nodes forward...
+//         p1 = p1.next;
+//         p2 = p2.next.next;
+//     }
+//     // When p2 reaches the last node, p1 will be exactly at the middle point...
+//     return p1;
+// };
 
 console.log("[1, 2, 3, 4, 5]", middleNode(head))
 console.log()
