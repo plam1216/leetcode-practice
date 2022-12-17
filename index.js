cd972c83e96ccadb43201ef5c6cb6cd7c13d8897
Peter Lam <plam1216@gmail.com>
2022-12-16 20:57:53 -0500

    shuffle array

--- index.js
+++ index.js
@@ -1079,3 +1079,31 @@ let mostWordsFound = (sentences) => {
 sentences = ["alice and bob love leetcode", "i think so too", "this is great thanks very much"]
 
 console.log("Most word in sentences:", mostWordsFound(sentences))
+console.log()
+
+
+/////////////////////////////////
+// 1470. Shuffle the Array (EASY)
+/////////////////////////////////
+// Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
+
+// Return the array in the form [x1,y1,x2,y2,...,xn,yn].
+
+// Example 1:
+// Input: nums = [2,5,1,3,4,7], n = 3
+// Output: [2,3,5,4,1,7] 
+// Explanation: Since x1=2, x2=5, x3=1, y1=3, y2=4, y3=7 then the answer is [2,3,5,4,1,7].
+
+let shuffle = (nums, n) => {
+    let arr = []
+    let yNums = nums.slice(n)
+
+    for (let i = 0; i < yNums.length; i++) {
+        arr.push(nums[i], yNums[i])
+    }
+
+    return arr
+};
+
+console.log("Shuffle Array:", shuffle([2, 5, 1, 3, 4, 7], 3))
+console.log()
